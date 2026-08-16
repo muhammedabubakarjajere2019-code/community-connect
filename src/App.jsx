e8f5d88ddfc35689f16199d75ded0a1004fe7609
muhammedabubakarjajere2019-code <muhammedabubakarjajere2019@gmail.com>
2026-08-16 11:43:35 +0100

    fix: remove BrowserRouter from App and duplicate route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./Pages/register";
 import Dashboard from "./Pages/dashboard";
 import Communities from "./Pages/communities";
 import CommunityDetails from "./Pages/communitydetails";
+import Members from "./Pages/members";
 import MemberDetails from "./Pages/memberdetails";
 import InviteMember from "./Pages/invitemember";
 import Events from "./Pages/events";
@@ -16,8 +17,9 @@ import CreateCommunity from "./Pages/createcommunity";
 import Profile from "./Pages/profile";
 import ForgotPassword from "./Pages/forgotpassword";
 import ResetPassword from "./Pages/resetpassword";
-
 import ProtectedRoute from './ProtectedRoute'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // <-- FIXED
+
 
 function App() {
   return (
@@ -25,164 +27,33 @@ function App() {
       <Routes>
 
         {/* PUBLIC PAGES */}
-
-        <Route
-          path="/"
-          element={<LandingPage />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-        <Route
-          path="/forgot-password"
-          element={<ForgotPassword />}
-        />
-
-        <Route
-          path="/reset-password"
-          element={<ResetPassword />}
-        />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
 
         {/* PROTECTED PAGES */}
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/communities" element={<ProtectedRoute><Communities /></ProtectedRoute>} />
+        <Route path="/create-community" element={<ProtectedRoute><CreateCommunity /></ProtectedRoute>} />
+        <Route path="/community/:id" element={<ProtectedRoute><CommunityDetails /></ProtectedRoute>} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/members" element={<ProtectedRoute><Members /></ProtectedRoute>} /> {/* <-- Only 1 now */}
+        
+        <Route path="/member/:id" element={<ProtectedRoute><MemberDetails /></ProtectedRoute>} />
+        <Route path="/invite-member" element={<ProtectedRoute><InviteMember /></ProtectedRoute>} />
 
-        <Route
-          path="/communities"
-          element={
-            <ProtectedRoute>
-              <Communities />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/events" element={<ProtectedRoute><Events /></ProtectedRoute>} />
+        <Route path="/create-event" element={<ProtectedRoute><CreateEvent /></ProtectedRoute>} />
+        <Route path="/event/:id" element={<ProtectedRoute><EventDetails /></ProtectedRoute>} />
 
-        <Route
-          path="/create-community"
-          element={
-            <ProtectedRoute>
-              <CreateCommunity />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/announcements" element={<ProtectedRoute><Announcements /></ProtectedRoute>} />
+        <Route path="/create-announcement" element={<ProtectedRoute><CreateAnnouncement /></ProtectedRoute>} />
+        <Route path="/announcement/:id" element={<ProtectedRoute><AnnouncementDetails /></ProtectedRoute>} />
 
-        <Route
-          path="/community/:id"
-          element={
-            <ProtectedRoute>
-              <CommunityDetails />
-            </ProtectedRoute>
-          }
-        />
-
-
-        <Route
-          path="/members"
-          element={
-            <ProtectedRoute>
-              <Members />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/member/:id"
-          element={
-            <ProtectedRoute>
-              <MemberDetails />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/invite-member"
-          element={
-            <ProtectedRoute>
-              <InviteMember />
-            </ProtectedRoute>
-          }
-        />
-
-
-        <Route
-          path="/events"
-          element={
-            <ProtectedRoute>
-              <Events />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/create-event"
-          element={
-            <ProtectedRoute>
-              <CreateEvent />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/event/:id"
-          element={
-            <ProtectedRoute>
-              <EventDetails />
-            </ProtectedRoute>
-          }
-        />
-
-
-        <Route
-          path="/announcements"
-          element={
-            <ProtectedRoute>
-              <Announcements />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/create-announcement"
-          element={
-            <ProtectedRoute>
-              <CreateAnnouncement />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/announcement/:id"
-          element={
-            <ProtectedRoute>
-              <AnnouncementDetails />
-            </ProtectedRoute>
-          }
-        />
-
-
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
 
       </Routes>
     </BrowserRouter>
