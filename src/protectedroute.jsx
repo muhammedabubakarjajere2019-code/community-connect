1d1f9fb8bea35ae89e596cac07c52abe17c59ecd
muhammedabubakarjajere2019-code <muhammedabubakarjajere2019@gmail.com>
2026-08-21 00:41:34 +0100

    Rename ProtectedRoute.jsx to protectedroute.jsx

---
+++ src/protectedroute.jsx
@@ -0,0 +1,1 @@
+﻿import { Navigate } from 'react-router-dom'; export default function ProtectedRoute({ children }) { const isAuth = localStorage.getItem('token'); return isAuth ? children : <Navigate to='/login' /> }
